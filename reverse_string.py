7f5124d9b52e513d9b26c4cca0cd93e469f45738
swinforb <swinforb@oregonstate.edu>
2021-06-06 11:14:56 -0700

    Fixed reverse_string

--- reverse_string.py
+++ reverse_string.py
@@ -9,9 +9,17 @@
 def main():
 	string = input("Please enter the string you wish to be reversed: ")
 	reversed_string = ""
+	string_arr = []
+	word = ""
 	for x in range(len(string)):
-		idx = len(string) - x - 1
-		reversed_string = reversed_string + string[idx]
+		if string[x] == " ":
+			string_arr.insert(0, word)
+			word = ""
+		else:
+			word = word + string[x]
+	string_arr.insert(0, word)
+	for y in range(len(string_arr)):
+		reversed_string = reversed_string + string_arr[y] + " "
 	print(reversed_string)
 	return reversed_string
 
